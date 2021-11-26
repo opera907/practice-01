7d1c0ddf99b5d21c383d7a8a62f568194352dcc7
dahee <opera9070@gmail.com>
2021-11-26 21:31:12 +0900

    feat : Create nav btn function

--- js/script.js
+++ js/script.js
@@ -1,2 +1,21 @@
+const nav = document.querySelector('.nav > ul')
+const li = document.querySelectorAll('.nav > ul li')
+const main = document.querySelector('main')
+
+nav.addEventListener('click', showContents)
 
 
+function showContents(e) {
+    const target = e.target
+    if(target.nodeName == "LI") {
+        //move li on class
+        li.forEach(ele => {
+            ele.classList.remove('on')
+            if(ele === target) ele.classList.add('on')
+        })
+        //show contents
+        const className = target.classList[0]
+        main.className = ""
+        main.classList.add(className)
+    }
+}
